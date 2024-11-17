07a429979c649dac2b614216952a8c09a7f4d396
almighty-k <takada.chopin.work@gmail.com>
2024-11-17 11:54:19 +0900

    chore: Set extend colors in tailwind config.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,23 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        black: {
+          DEFAULT: "#141416",
+          secondary: "#222529",
+        },
+        white: {
+          DEFAULT: "#E7ECF3",
+          secondary: "#B1B5C3",
+        },
+        green: {
+          DEFAULT: "#58C27D",
+        },
+        yellow: {
+          DEFAULT: "#FFAF4E",
+        },
+        purple: {
+          DEFAULT: "#878CFF",
+        },
       },
     },
   },
