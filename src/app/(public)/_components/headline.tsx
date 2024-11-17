8de5217cf099d21d5d81114415a9d71b61b8086b
almighty-k <takada.chopin.work@gmail.com>
2024-11-17 16:34:26 +0900

    refactor: Refactor Headline components

--- src/app/(public)/_components/headline.tsx
+++ src/app/(public)/_components/headline.tsx
@@ -8,9 +8,9 @@ export function Headline({ title, subtitle }: HeadlineProps) {
     <div className="flex flex-col gap-2">
       <div>
         <h1 className="text-center text-4xl font-bold text-white">{title}</h1>
-        {subtitle && (
+        {subtitle ? (
           <p className="text-center text-white-secondary">{subtitle}</p>
-        )}
+        ) : null}
       </div>
       <div className="mx-auto h-1 w-24 rounded-lg bg-yellow" />
     </div>
