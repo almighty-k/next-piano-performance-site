b07124fad060545722c451ece5e65ef554023518
almighty-k <takada.chopin.work@gmail.com>
2024-11-17 16:43:45 +0900

    update: Add no environments error handling in client.ts

--- src/app/(public)/_libs/client.ts
+++ src/app/(public)/_libs/client.ts
@@ -1,7 +1,15 @@
 import { createClient } from "microcms-js-sdk";
 import process from "node:process";
 
+if (!process.env.SERVICE_DOMAIN) {
+  throw new Error("SERVICE_DOMAIN  is not set");
+}
+
+if (!process.env.API_KEY) {
+  throw new Error("API_KEY is not set");
+}
+
 export const client = createClient({
-  serviceDomain: process.env.SERVICE_DOMAIN!,
-  apiKey: process.env.API_KEY!,
+  serviceDomain: process.env.SERVICE_DOMAIN,
+  apiKey: process.env.API_KEY,
 });
